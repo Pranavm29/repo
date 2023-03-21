efeaf803ccb350a1bf5a670d3dabbf6f384d5a17
Pranavm29 <86351097+Pranavm29@users.noreply.github.com>
2023-03-21 16:43:17 +0530

    Update simple12.py

--- simple12.py
+++ simple12.py
@@ -1,9 +1,11 @@
-#Python program to add two numbers using function
+import sys
 
-def add_num(a,b):#function for addition
-    sum=a+b;
-    return sum; #return value
-num1=int(input("input the number one: "))#input from user for num1
-num2=int(input("input the number one :"))#input from user for num2
+def add_num(a, b): # function for addition
+    sum = a + b
+    return sum # return value
 
-print("The sum is",add_num(num1,num2))#call te function
+num1 = int(sys.argv[1]) # first argument is num1
+num2 = int(sys.argv[2]) # second argument is num2
+
+print("The sum is", add_num(num1, num2)) # call the function and print the result
+
