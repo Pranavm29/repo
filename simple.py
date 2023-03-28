b8e5a4453c98a76a7cae759f813a4f64c7721696
Pranavm29 <86351097+Pranavm29@users.noreply.github.com>
2023-03-28 17:02:24 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -2,8 +2,10 @@
 
 def add_num(a,b):#function for addition
     sum=a+b;
-    return sum; #return value
-
+    return sum;#return value
+def sub num(a,b):
+    subtraction = a-b
+    return subtraction
 num1=205  #variable declaration
 num2=55
 print("The sum is",add_num(num1,num2))#call the function
